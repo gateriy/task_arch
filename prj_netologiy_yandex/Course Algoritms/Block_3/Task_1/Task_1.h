d580b835aa3cda39fc2c4874a8d56447dd3ad634
Nick Fedoseev <gateriy@yandex.ru>
2024-10-09 23:55:18 +0300

    Task  09.10.24

---
+++ prj_netologiy_yandex/Course Algoritms/Block_3/Task_1/Task_1.h
@@ -0,0 +1,12 @@
+//#pragma once
+#ifndef TASK_1_H
+#define TASK_1_H
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+void Task_1();
+int quick_sort(int* arr, int size);
+
+#endif 
---
+++ prj_netologiy_yandex/Course Algoritms/Block_4/Task_1_3_(arr_din_logic)/Task_1_3_(arr_din_logic).cpp
@@ -0,0 +1,13 @@
+п»ї// Task_1_3_(arr_din_logic)
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+#include"AllConfigTask.h"
+
+int main() {
+
+	AllConfigTask config_task;
+	config_task.InteractiveVarTask("Input numder task Dinamick Array (1, 2, 3) or \"0\" exit: ");
+
+}
